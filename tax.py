e8e73f2ce185e55e8769ba14b4b86910fc154dfa
Dante Dobson-Davies <144271311+DantesPhoenix@users.noreply.github.com>
2024-02-09 10:11:28 +0000

    Update tax.py
    small tweakes

--- tax.py
+++ tax.py
@@ -1,49 +1,25 @@
 def calculate_tax(income):
   if income <= 12571:
       tax_amount = 0
-    
   elif income <= 14876:
-      tax_amount = (income - 12571) * 0.19
-    
+      tax_amount = (income - 12500) * 0.19
   elif income <= 26561:
-      tax_amount = (income - 14876) * 0.20 + (14876 - 12571) * 0.19
-    
+      tax_amount = (income - 14585) * 0.20 + (14585 - 12500) * 0.19
   elif income <= 43662:
-      tax_amount = (income - 26561) * 0.21 + (26561 - 14876) * 0.20 + (14876 - 12571) * 0.19
-    
+      tax_amount = (income - 25158) * 0.21 + (25158 - 14585) * 0.20 + (14585 - 12500) * 0.19
   elif income <= 75000:
-      tax_amount = (income - 43662) * 0.41 + (43662 - 26561) * 0.21 + (26561 - 14876) * 0.20 + (14876 - 12571) * 0.19
-    
+      tax_amount = (income - 43430) * 0.42 + (43430 - 25158) * 0.21 + (25158 - 14585) * 0.20 + (14585 - 12500) * 0.19
   elif income <= 125140:
-      tax_amount = (income - 75000) * 0.46 + (75000 - 43662) * 0.41 + (43662 - 26561) * 0.21 + (26561 - 14876) * 0.20 + (14876 - 12571) * 0.19
-    
-  elif income > 125140:
-      tax_amount = (income - 125140) + (125140 - 75000) * 0.46 + (75000 - 43662) * 0.41 + (43662 - 26561) * 0.21 + (26561 - 14876) * 0.20 + (14876 - 12571) * 0.19
-    
+      tax_amount = (income - 75000) * 0.45 + (75000 - 43430) * 0.42 + (43430 - 25158) * 0.21 + (25158 - 14585) * 0.20 + (14585 - 12500) * 0.19
+  else:
+    tax_amount = (income - 125140) * 0.48 + (125140 - 75000) * 0.45 + (75000 - 43430) * 0.42 + (43430 - 25158) * 0.21 + (25158 - 14585) * 0.20 + (14585 - 12500) * 0.19
+  
 
   return tax_amount
 
 def calcBrackets(income):
-  if income <= 12571:
-    print("You are in the no tax bracket", )
-
-  elif income <= 14876:
-    print("You are in the 'Starter Rate' tax bracket", )
-
-  elif income <= 26561:
-    print("You are in the 'Scottish Basic Rate' tax bracket", )
-
-  elif income <= 43662:
-    print("You are in the 'Intermediate Rate' tax bracket", )
-
-  elif income <= 75000:
-    print("You are in the 'Higher Rate' tax bracket", )
-
-  elif income <= 125140:
-    print("You are in the 'Advanced Rate' tax bracket", )
-
-  elif income > 125140:
-    print("You are in the 'Top Rate' tax bracket", )
+    if income <= 12500:
+      print("your ta")
 
 # Get user input for income
 income = float(input("Enter your income: "))
@@ -57,3 +33,4 @@ calcBrackets(income)
 # Print the tax amount
 print("The tax you pay is: £", tax_amount)
 print("Your take home pay is: £", income - tax_amount)
+print("your monthly pay is: ", income - tax_amount/12)
